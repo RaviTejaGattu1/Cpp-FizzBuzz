a040dd0152e296ad33353f8adf4c35265c34b885
rtlyf217 <46156268+rtlyf217@users.noreply.github.com>
2019-01-30 11:50:41 +0530

    Add files via upload

---
+++ FB.cpp
@@ -0,0 +1,44 @@
+//g++  5.4.0
+
+#include <iostream>
+
+int main()
+{
+    cout << "Hello, world!\n";
+    cout<<"enter the counter and values in next line"<<"\n";
+    int a,i,j;
+    cin>>a;
+	int z;
+	z=a-1;
+    if ((a>0)&&(a<11))
+        
+    {   int arra[z];
+        
+    while (j<a)
+    {
+        
+    for(j=0;j<a;j++)
+        
+    {
+    cin>>arra[j];
+    }
+    
+    
+    }	
+	
+	cout<<"no of entries"<<a<<"\n";
+    cout<<"and their values are"<<"\n";
+    for(i=0;i<a;i++)
+    {
+    cout<<arra[i]<<"\n";
+    
+    }
+	
+	
+     }
+    
+    else 
+	{cout<<"invalid input";}
+    
+    
+}
